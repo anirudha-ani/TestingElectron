694f4d7c4878825ac8b9db4088500b77d88facd2
Anirudha Paul <anirudhaprasun@gmail.com>
2020-12-14 19:10:14 +0600

    Creating a static server to send file to other devices. Attached the link to the QR code

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const { app, BrowserWindow } = require('electron')
 const electron = require('electron')
 const ipc = electron.ipcMain
+const ip = require('ip');
+const { fork } = require('child_process')
+const ps = fork(`${__dirname}/server.js`)
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
@@ -10,10 +14,18 @@ function createWindow() {
         }
     })
 
+    const port = 9990;
+    const serverIp = `http://${ip.address()}:${port}`
+
+    //Change this file name to your desired 
+    const fileName = "photo.jpg"
+
+    const linkForQRCode = `${serverIp}/${fileName}`
+
     win.loadFile('index.html')
     
     ipc.on('generate-qr', function (event) {
-        event.sender.send('url-for-qr','Mamma mia')
+        event.sender.send('url-for-qr',`${linkForQRCode}`)
     })
 }
 
